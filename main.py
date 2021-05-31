cf9cdb8ebd4a207edd3534592fc07effb072253d
hernu123 <estheranuoluwapo.oduntan@gmail.com>
2021-06-01 00:58:52 +0300

    fith commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-nuumber = int(input("Enter a natural number : "))
+number = int(input("Enter a natural number : "))
 if number < 1:
     print("Number needs to be greater than 1")
 elif number == 1:
     print(number, " is neigher prime nor composite")
 else:
-    for divisor in range(2, (number//2))
+    for divisor in range(2, (number//2)+1):
         if (number % divisor) == 0:
-            print(number, "is a composite Number")
+            print(number, "is a Composite Number")
+            break
+        else:
+            print(number, "is a Prime Number")
