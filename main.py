c55d0647140625b58d0fc87f3128f024f61e2c4c
hernu123 <estheranuoluwapo.oduntan@gmail.com>
2021-06-01 01:01:42 +0300

    sixth commit

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ elif number == 1:
     print(number, " is neigher prime nor composite")
 else:
     for divisor in range(2, (number//2)+1):
-        if (number % divisor) == 0:
-            print(number, "is a Composite Number")
-            break
-        else:
-            print(number, "is a Prime Number")
+       print(divisor)
