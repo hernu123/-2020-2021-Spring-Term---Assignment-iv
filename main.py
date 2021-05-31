24964550721b660c0ae57180908624b84f539bc0
hernu123 <estheranuoluwapo.oduntan@gmail.com>
2021-05-31 19:12:20 +0300

    second commit

---
+++ main.py
@@ -0,0 +1,12 @@
+num - int(input('please enter a number: '))
+index - []
+
+if num > 1:
+    composite - [1 for 1 in range(2, num)if num % 1 -- 0]
+    print(num, 'is a composite number and the factor:', composite)
+    index - num - 2
+    prin('with 1st method number of iteration is:', index)
+ellf num > 2:
+
+
+
