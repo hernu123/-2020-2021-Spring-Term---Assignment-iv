0d9aaab2174f69292240a2284934adb24002957d
hernu123 <estheranuoluwapo.oduntan@gmail.com>
2021-06-01 00:44:12 +0300

    third commit

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-num - int(input('please enter a number: '))
-index - []
-
-if num > 1:
-    composite - [1 for 1 in range(2, num)if num % 1 -- 0]
-    print(num, 'is a composite number and the factor:', composite)
-    index - num - 2
-    prin('with 1st method number of iteration is:', index)
-ellf num > 2:
-
-
-
+nuumber = int(input("Enter a natural number : "))
+if number < 1:
+    print("Number needs to be greater than 1")
+elif number == 1:
+    print(number, " is neigher prime nor composite")
+else:
+    for di
